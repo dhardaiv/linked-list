f7f29eaf3a9d7cfdcbeb6f0ffbf00549b1a1b038
dhardaiv <vikdai217@gmail.com>
2024-06-25 00:57:24 -0400

    cooked

--- q3.c
+++ q3.c
@@ -8,10 +8,10 @@ void printDuplicates(Node *head) {
   Node *current = head; //start at head 
 
   while (current != NULL) { //check null
-    Node *runner = current->next;
+    Node *runner = current->next; //create a runner to check two values at once
     while (runner != NULL) {
-      if (current->data == runner->data) {
-        printf("%d\n", current->data);
+      if (current->data == runner->data) { //check if eequal
+        printf("%d\n", current->data);//print out redundant value
       }
 
       current->next = runner->next;
