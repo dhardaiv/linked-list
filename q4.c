638f53b6459f1b324fc6e449a6ea18809646af6d
dhardaiv <vikdai217@gmail.com>
2024-06-25 01:21:40 -0400

    cooked

---
+++ q4.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+typedef struct node {
+  int value;
+  struct node* link;
+} Node;
+
+void simplify(Node *head) {
+    Node *current;
+    head = current; 
+    if (current = NULL){
+        return;
+    }
+
+    while (current->link != NULL){
+        if (current->data == current->link->data) {
+            Node *remove = current->link;
+            current->link = current->link->link
+            free(remove);
+        } else {
+            current = current->link;
+        }
+    }
+}
